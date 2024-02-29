4bc5a59dad0a149d4b49e5ff7b0cfcd0c77a9d03
giorgio sancarlo <sancarlogiorgio@gmail.com>
2024-02-29 13:58:13 +0100

    business-logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+interface Product {
+  qty: number;
+  userId: number;
+  title: string;
+  description: string;
+  id: number;
+  price: number;
+  image: string;
+  thumbnail: string;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [products, setProducts] = useState<Array<Product>>([]);
+  const [cart, setCart] = useState<
+    Array<{
+      product: Product;
+      qty: number;
+    }>
+  >([]);
+  const [paid, setPaid] = useState<boolean>(false);
+
+  function addToCart(product: Product) {
+    const productFound = cart.find(
+      (productCart) => productCart.product.id === product.id
+    );
+    if (!!productFound) {
+      const newCart = cart.map((productCart) =>
+        productCart.product.id === product.id
+          ? { product: productCart.product, qty: productCart.qty + 1 }
+          : { ...productCart }
+      );
+      setCart(newCart);
+    } else setCart([...cart, { product, qty: 1 }]);
+  }
+
+  function removeFromCart(id: number) {
+    const productFound = cart.find(
+      (productCart) => productCart.product.id === id
+    );
+    if (productFound && productFound.qty > 1) {
+      const newCart = cart.map((productCart) =>
+        productCart.product.id === id
+          ? { product: productCart.product, qty: productCart.qty - 1 }
+          : { ...productCart }
+      );
+      setCart(newCart);
+    } else {
+      const newCart = cart.filter(
+        (productCart) => productCart.product.id !== id
+      );
+      setCart(newCart);
+    }
+  }
+
+  function pay() {
+    setPaid(true);
+    setCart([]);
+  }
+
+  return <h1>Hello</h1>;
 }
 
 export default App;
